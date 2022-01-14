001207a03a0396c37e4996b9b8c8120312b30288
Darren Barklie <darrenbarklie@gmail.com>
2022-01-14 21:08:20 +0000

    Import, export modules

--- 10-modules-import-export.js
+++ 10-modules-import-export.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import { sumTwo as sumTwoNumbers, sumThree } from "./10-addition.js";
+import * as subtraction from "./10-subtraction.js";
+// import * as _ from 'lodash';
+
+console.log("2 + 3 = " + sumTwoNumbers(2, 3));
+console.log("2 + 3 + 4 = " + sumThree(2, 3, 4));
+
+console.log("3 - 2 = " + subtraction.subtractTwo(3, 2));
+console.log("10 - 3 - 2 = " + subtraction.subtractThree(10, 3, 2));
---
+++ 10-subtraction.js
@@ -0,0 +1,9 @@
+function subtractTwo(a, b) {
+  return a - b;
+}
+
+function subtractThree(a, b, c) {
+  return a - b - c;
+}
+
+export { subtractTwo, subtractThree };
